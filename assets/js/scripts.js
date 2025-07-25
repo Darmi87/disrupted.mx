3fecd011188e0c75fbf0284d5f786369a2284c9f
unknown <damianrye87@gmail.com>
2025-07-25 16:21:15 -0300

    Agregado hover en redes sociales de talentos

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,26 +1,33 @@
-const talentos = [
-  { nombre: "Creador 1", imagen: "assets/img/creador1.jpg" },
-  { nombre: "Creador 2", imagen: "assets/img/creador2.jpg" },
-  { nombre: "Creador 3", imagen: "assets/img/creador3.jpg" },
-  { nombre: "Creador 4", imagen: "assets/img/creador4.jpg" },
-  // Agregá más talentos aquí
+const talents = [
+  { name: "Aldo", image: "assets/img/talent-images/aldo.jpg", link: "talents/aldo.html" },
+  { name: "Antrax", image: "assets/img/talent-images/antrax.jpg", link: "talents/antrax.html" },
+  { name: "Yetus", image: "assets/img/talent-images/yetus.jpg", link: "talents/yetus.html" },
+  { name: "Barbie", image: "assets/img/talent-images/barbie.jpg", link: "talents/barbie.html" },
+  { name: "Danyan", image: "assets/img/talent-images/danyan.jpg", link: "talents/danyan.html" },
+  { name: "Josue", image: "assets/img/talent-images/josue.jpg", link: "talents/josue.html" },
+  { name: "Juan", image: "assets/img/talent-images/juan.jpg", link: "talents/juan.html" },
+  { name: "Yair", image: "assets/img/talent-images/yair.jpg", link: "talents/yair.html" },
+  { name: "Karma", image: "assets/img/talent-images/karma.jpg", link: "talents/karma.html" },
+  { name: "Magui Sunshine", image: "assets/img/talent-images/magui.jpg", link: "talents/magui-sunshine.html" },
+  { name: "Scarday", image: "assets/img/talent-images/scar.jpg", link: "talents/scarday.html" },
+  { name: "Angel soy yo", image: "assets/img/talent-images/angel.jpg", link: "talents/angel-soy-yo.html" },
+  { name: "Erkvaldo", image: "assets/img/talent-images/erk.jpg", link: "talents/erkvaldo.html" },
+  { name: "Kassandra", image: "assets/img/talent-images/kasandra.jpg", link: "talents/kassandra.html" },
+  { name: "Merulah", image: "assets/img/talent-images/meru.jpg", link: "talents/merulah.html" },
+  { name: "Boomsniper", image: "assets/img/talent-images/boom.jpg", link: "talents/boomsniper.html" }
 ];
 
-function shuffle(array) {
-  return array.sort(() => Math.random() - 0.5);
-}
+const shuffledTalents = talents.sort(() => Math.random() - 0.5);
+const grid = document.getElementById("talentGrid");
 
-function renderTalents() {
-  const grid = document.getElementById("talentGrid");
-  shuffle(talentos).forEach(t => {
-    const card = document.createElement("div");
-    card.className = "card";
-    card.innerHTML = `
-      <img src="${t.imagen}" alt="${t.nombre}" />
-      <p>${t.nombre}</p>
-    `;
-    grid.appendChild(card);
-  });
-}
-
-renderTalents();
+shuffledTalents.forEach(talent => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.innerHTML = `
+    <a href="${talent.link}">
+      <img src="${talent.image}" alt="${talent.name}">
+      <p>${talent.name}</p>
+    </a>
+  `;
+  grid.appendChild(card);
+});
